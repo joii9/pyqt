375039a332fc3f417be832a39c2802016a344a92
joii9 <joel.c.mz9@gmail.com>
2025-02-27 17:19:30 -0600

    I have added a Vertical Layout and a label too

--- test.py
+++ test.py
@@ -4,8 +4,16 @@ import PyQt5.QtWidgets as qtw
 class MainWindow(qtw.QWidget):
      def __init__(self):
          super().__init__()
+         
          #Add title
          self.setWindowTitle("Hello world")
+
+         #Set a Vertical layout
+         self.setLayout(qtw.QVBoxLayout()) #If we want a Horizontal Layout we need to exchange QVBoxLayout to QHBoxLayout
+
+         #Create a Label
+         my_label= qtw.QLabel("Joel Carbajal Muñoz")
+         self.layout().addWidget(my_label)
 
          self.show()
 
